04c1d284121ceb678196832ef2909a1ffb9dc0d5
suljaxm <ujxhz@outlook.com>
2019-05-16 18:14:32 +0800

    complement the function of predict, update, updateEKF

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,16 +26,82 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+
+  
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  float px, py, vx, vy, ro, phi, ro_dot;
+  VectorXd z_pred(3);
+
+  px = x_(0);
+  py = x_(1);
+  vx = x_(2);
+  vy = x_(3);
+
+  // convert current state into polar coordinate
+  ro = sqrt(px * px + py * py);
+  phi = atan2(py, px);
+
+  if (fabs(ro) > 0.01) {
+    ro_dot = (px * vx + py * vy) / ro;
+  }
+  else{ // make z_pred 0.0 if too small
+    ro = 0.0;
+    phi = 0.0;
+    ro_dot = 0.0; 
+  }
+
+  
+  z_pred << ro, phi, ro_dot;
+  VectorXd y = z - z_pred;
+
+  // Normalize the angle, so that angle is between -pi and pi
+  if (y(1)>M_PI) {
+    y(1) -= 2*M_PI;
+  }
+  if (y(1)<-M_PI) {
+    y(1) += 2*M_PI;
+  }
+  
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+
 }
